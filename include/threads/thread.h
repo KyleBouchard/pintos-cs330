51a5e1ef3932b90322f2c720288d99597603a829
root <edelina.alieva@polymtl.ca>
2024-10-04 06:45:49 +0000

    Fix header

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -97,7 +97,7 @@ struct thread {
 	struct lock *blocked_on;            /* Lock currently blocked on. */
 
 	/* Shared between thread.c and synch.c. */
-	struct list_elem elem;              /* List element. */ */
+	struct list_elem elem;              /* List element. */
 
 #ifdef USERPROG
 	/* Owned by userprog/process.c. */
