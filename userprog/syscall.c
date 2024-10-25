f3055ac90e633aa4a24c577a66c0d05b96de70c7
Kyle Bouchard <kylecbouchard@gmail.com>
2024-10-25 03:50:54 +0000

    Add syscall declarations

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -5,12 +5,30 @@
 #include "threads/thread.h"
 #include "threads/loader.h"
 #include "userprog/gdt.h"
+#include "userprog/process.h"
 #include "threads/flags.h"
 #include "threads/init.h"
 #include "intrinsic.h"
 
+typedef int pid_t;
+
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
+
+void halt(void);
+void exit(int status);
+pid_t fork(const char *thread_name);
+int exec(const char *cmd_line);
+int wait(pid_t pid);
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
+int open(const char *file);
+int filesize(int fd);
+int read(int fd, void *buffer, unsigned size);
+int write(int fd, const void *buffer, unsigned size);
+void seek(int fd, unsigned position);
+unsigned tell(int fd);
+void close(int fd);
 
 /* System call.
  *
@@ -107,7 +125,7 @@ halt (void) {
 
 void 
 exit (int status) {
-
+	
 }
 
 pid_t
